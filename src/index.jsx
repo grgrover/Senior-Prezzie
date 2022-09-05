302f96891ee4a7aea83eb44894f54d51e7cfdeaf
Greta Grover <greta.rgrover@gmail.com>
2022-09-05 10:13:40 -0400

    Ready for Prezzie

--- src/index.jsx
+++ src/index.jsx
@@ -13,12 +13,7 @@ import Grow from '@mui/material/Grow';
 
 
 export default function MediaCard() {
-const [checked, setChecked] = useState(false)
 
-
-  useEffect(() => {
-    setChecked(true)
-  }, [])
 
   return (
 
@@ -27,11 +22,6 @@ const [checked, setChecked] = useState(false)
       direction='column'
       alignItems='center'
       justify='center'>
-    <Grow
-        in={checked}
-        style={{ transformOrigin: '0 0 0' }}
-        {...(checked ? { timeout: 1000 } : {})}
-      >
         <CardMedia
           component="img"
           height="700"
@@ -39,14 +29,12 @@ const [checked, setChecked] = useState(false)
           image="https://cdn.mos.cms.futurecdn.net/VsfnyTLzYvxJudvc2rb3uH.jpg"
           alt="tom"
         />
-      </Grow>
-
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
- SPIDERMAN IS THE BEST SUPERHERO
+          Tom Holland IS THE BEST SPIDERMAN
         </Typography>
         <Typography variant="body2" color="text.secondary">
-        Tom Holland perfectly captures the essence of a high school student struggling to balance his schoolwork, personal relationships, and responsibility as a superhero all in one. With a strong background in dance and gymnastics, Holland’s skills at backflipping and somersaulting also made him perfect for the part.Tobey Maguire gave audiences the best and most accurate version of Peter Parker. He perfectly illustrated both Spider-Man’s inner anxieties as well as his transformative superhero personality.
+
         </Typography>
       </CardContent>
     </Grid>
