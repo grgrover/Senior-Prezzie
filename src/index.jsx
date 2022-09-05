7cbe90eed2069914e7219aa4b7a31f3a5d1148b9
Greta Grover <greta.rgrover@gmail.com>
2022-09-05 10:15:37 -0400

    Tom with transitions

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,11 @@ import Grow from '@mui/material/Grow';
 
 
 export default function MediaCard() {
+  const [checked, setChecked] = useState(false)
+
+  useEffect(() => {
+    setChecked(true)
+  }, [])
 
 
   return (
@@ -22,6 +27,11 @@ export default function MediaCard() {
       direction='column'
       alignItems='center'
       justify='center'>
+      <Grow
+        in={checked}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(checked ? { timeout: 1000 } : {})}
+      >
         <CardMedia
           component="img"
           height="700"
@@ -29,11 +39,15 @@ export default function MediaCard() {
           image="https://cdn.mos.cms.futurecdn.net/VsfnyTLzYvxJudvc2rb3uH.jpg"
           alt="tom"
         />
+      </Grow>
+
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Tom Holland IS THE BEST SPIDERMAN
         </Typography>
         <Typography variant="body2" color="text.secondary">
+
+          Tom Holland perfectly captures the essence of a high school student struggling to balance his schoolwork, personal relationships, and responsibility as a superhero all in one. With a strong background in dance and gymnastics, Holland’s skills at backflipping and somersaulting also made him perfect for the part.
 
         </Typography>
       </CardContent>
