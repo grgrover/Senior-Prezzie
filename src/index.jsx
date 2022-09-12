b43c4dac75891ca7aea4fbd04a64d66f7ae3b10e
Greta Grover <greta.rgrover@gmail.com>
2022-09-12 15:28:23 -0400

    Tom with transitions

--- src/index.jsx
+++ src/index.jsx
@@ -11,13 +11,11 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 
 
-
 export default function MediaCard() {
-
-
+  const [checked, setChecked] = useState(false)
 
   useEffect(() => {
-
+    setChecked(true)
   }, [])
 
   return (
@@ -27,19 +25,29 @@ export default function MediaCard() {
       direction='column'
       alignItems='center'
       justify='center'>
-      <CardMedia
-        component="img"
-        height="700"
-        width="auto"
-        image="https://cdn.mos.cms.futurecdn.net/VsfnyTLzYvxJudvc2rb3uH.jpg"
-        alt="tom"
-      />
+      <Grow
+        in={checked}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(checked ? { timeout: 1000 } : {})}
+      >
+        <CardMedia
+          component="img"
+          height="700"
+          width="auto"
+          image="https://cdn.mos.cms.futurecdn.net/VsfnyTLzYvxJudvc2rb3uH.jpg"
+          alt="tom"
+        />
+      </Grow>
+
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
 
           TOM HOLLAND IS THE BEST SUPERMAN
         </Typography>
         <Typography variant="body2" color="text.secondary">
+
+          Tom Holland perfectly captures the essence of a high school student struggling to balance his schoolwork, personal relationships, and responsibility as a superhero all in one. With a strong background in dance and gymnastics, Holland’s skills at backflipping and somersaulting also made him perfect for the part.
+
         </Typography>
       </CardContent>
     </Grid>
